18062ebeb216f34f5887b9c83f8ef04c6262a994
Joe Pearson <joe.pearson@dlr.de>
2019-01-22 16:58:12 +0100

    Add OpenFOAM part

--- init.el
+++ init.el
@@ -223,7 +223,24 @@
 )
  
 (defun openfoam-hgw-c-mode-hook ()
+  "OpenFOAM C++ style."
   (c-set-style "OpenFOAM_HGW"))
 ;; (add-hook 'c-mode-common-hook 'openfoam-hgw-c-mode-hook)
+
+;; backup files in emacs dict
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;;; init.el ends here
