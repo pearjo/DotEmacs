b05e3889ff9ade31eea8bbefeb99a6684cf21c86
Joe Pearson <joe.pearson@dlr.de>
2019-04-09 20:57:40 +0200

    Update dired mode
    Reuse dired buffers.

--- init.el
+++ init.el
@@ -318,17 +318,45 @@
   (kill-emacs))
 
 ;; Dired
-(use-package all-the-icons-dired
-  :ensure t
-  :config
-  (setq dired-listing-switches "-lGh1v --group-directories-first")
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+;; use only one buffer for dired
+(defadvice dired-advertised-find-file (around dired-subst-directory activate)
+  "Replace current buffer if file is a directory."
+  (interactive)
+  (let ((orig (current-buffer))
+        (filename (dired-get-filename)))
+    ad-do-it
+    (when (and (file-directory-p filename)
+               (not (eq (current-buffer) orig)))
+      (kill-buffer orig))))
+
+(eval-after-load "dired"
+  ;; don't remove `other-window', the caller expects it to be there
+  '(defun dired-up-directory (&optional other-window)
+     "Run Dired on parent directory of current directory."
+     (interactive "P")
+     (let* ((dir (dired-current-directory))
+     	    (orig (current-buffer))
+     	    (up (file-name-directory (directory-file-name dir))))
+       (or (dired-goto-file (directory-file-name dir))
+     	   ;; Only try dired-goto-subdir if buffer has more than one dir.
+     	   (and (cdr dired-subdir-alist)
+     		(dired-goto-subdir up))
+     	   (progn
+     	     (kill-buffer orig)
+     	     (dired up)
+     	     (dired-goto-file dir))))))
+
+(setq dired-listing-switches "-lGh1v --group-directories-first")
+(global-auto-revert-mode 1)
 
 (add-hook 'dired-mode-hook
           (lambda ()
             (local-set-key (kbd "<dead-circumflex>") 'dired-up-directory)))
 
-(global-auto-revert-mode 1)
+(use-package all-the-icons-dired
+  :ensure t
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
 
 ;; Column length waring
 (require 'whitespace)
