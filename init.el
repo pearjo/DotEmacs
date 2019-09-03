a42a06edbb2f9e9bfaa4a2f70dffee1d340c39bc
Joe Pearson <joe-nicholas.pearson.sp@lht.dlh.de>
2019-09-03 08:58:04 +0200

    Update irony settings to improve performance

--- init.el
+++ init.el
@@ -697,6 +697,13 @@ Switch between English and German."
   :config
   (add-to-list 'company-backends 'company-irony-c-headers))
 
+(when (boundp 'w32-pipe-read-delay)
+  (setq w32-pipe-read-delay 0))
+
+;; Set the buffer size to 64K on Windows (from the original 4K)
+(when (boundp 'w32-pipe-buffer-size)
+  (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
+
 (defun create-etags (dir)
   "Create tags file for source files in DIR."
   (interactive "Ddirectory: ")
