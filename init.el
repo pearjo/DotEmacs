8f726de678ce25aaae83a85c022dc434b9d7054b
Joe Pearson <pearjo@protonmail.com>
2019-11-07 20:33:20 +0100

    solarized: Update solarized theme settings

--- init.el
+++ init.el
@@ -153,13 +153,27 @@
   :ensure t
   :defer
   :config
+  ;; Don't change the font for some headings and titles
   (setq solarized-use-variable-pitch nil)
-  (setq solarized-use-less-bold t)
+
+  ;; Use more italics
   (setq solarized-use-more-italic t)
+
+  ;; Use less colors for indicators such as git:gutter, flycheck and
+  ;; similar
   (setq solarized-emphasize-indicators nil)
+
+  ;; Don't change size of org-mode headlines (but keep other
+  ;; size-changes)
   (setq solarized-scale-org-headlines nil)
-  (setq solarized-high-contrast-mode-line nil)
-  (setq solarized-distinct-fringe-background nil)
+
+  ;; make the modeline high contrast
+  (setq solarized-high-contrast-mode-line t)
+
+  ;; make the fringe stand out from the background
+  (setq solarized-distinct-fringe-background t)
+
+  ;; Avoid all font-size changes
   (setq solarized-height-minus-1 1.0)
   (setq solarized-height-plus-1 1.0)
   (setq solarized-height-plus-2 1.0)
