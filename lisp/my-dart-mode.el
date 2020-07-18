e22637717c10e8a1184e85bb82a558930377076a
Joe Pearson <pearjo@protonmail.com>
2020-07-18 13:12:44 +0200

    dart: Add lsp to dart mode
    Use the lsp mode and setup with basic settings like pointing to the
    SDK directory.

--- lisp/my-dart-mode.el
+++ lisp/my-dart-mode.el
@@ -6,18 +6,33 @@
 ;; Keywords: dart
 
 ;;; Commentary:
+;;
+;;  Note that there is a bug in the
+;;  `lsp-dart-flutter-daemon--running-p' function of the
+;;  lsp-dart-daemon.  Use `get-buffer' to check if a buffer already
+;;  excists.
 
 ;;; Code:
 (require 'use-package)
 
-(use-package dart-mode
+(use-package lsp-mode :ensure t)
+(use-package lsp-dart
   :ensure t
-  :init
-  (add-hook 'dart-mode-hook 'eglot-ensure))
+  :hook (dart-mode . lsp)
+  :custom
+  (lsp-dart-sdk-dir "/home/joe/flutter/bin/cache/dart-sdk"))
+
+;; Optional Flutter packages
+(use-package hover :ensure t) ;; run app from desktop without emulator
+
+(use-package dart-mode
+  :ensure t)
 
 (use-package flutter
   :ensure t
-  :bind ("C-M-x" . #'flutter-run-or-hot-reload))
+  :bind ("C-M-x" . #'flutter-run-or-hot-reload)
+  :custom
+  (flutter-sdk-path "/home/joe/flutter"))
 
 (provide 'my-dart-mode)
 ;;; my-dart-mode.el ends here
